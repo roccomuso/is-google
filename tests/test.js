d7d6433d589e51e3ecda1f7c19ca08d2b6e1adf1
roccomuso <roccomusolino92@gmail.com>
2018-04-29 14:59:12 +0200

    improved tests

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,17 @@
 const test = require('tape')
 const isGoogle = require('../')
 
-test('should fail on wrong ip', (t) => {
-  t.plan(1)
+test('should fail on wrong IPs', (t) => {
+  t.plan(2)
   isGoogle('1.1.1.1').then(outcome => t.notOk(outcome))
+  isGoogle('123.123.123.123').catch(err => t.equal(err.code, 'ENOTFOUND'))
 })
 
 test('should fail with wrong inputs', (t) => {
-  t.plan(2)
+  t.plan(3)
   isGoogle('helloworld').catch(err => t.ok(err))
   isGoogle('0.0.0.0.0.0').catch(err => t.ok(err))
+  isGoogle().catch(err => t.ok(err))
 })
 
 test('should pass on valid googlebot.com crawler ip', (t) => {
